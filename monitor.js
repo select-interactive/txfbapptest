dd1ab06bf3c38bf57cf7e993ceaaadd9c9ac5cc3
Dan Harris <danh@select-interactive.com>
2022-06-03 08:44:54 -0500

    20ms spawn time

--- monitor.js
+++ monitor.js
@@ -1,13 +1,13 @@
 const { io } = require("socket.io-client");
 
-const URL = process.env.URL || 'https://texas-football-scores-sockets.azurewebsites.net'; // "http://localhost:3001";
+const URL = process.env.URL || "http://localhost:3001"; // 'https://texas-football-scores-sockets.azurewebsites.net'; 
 
 
 const createClient = () => {
 
 
     const transports = [ "websocket" ];
-    const socket = io( `${ URL }/games?gameId=${ gameId }`, {
+    const socket = io( `${ URL }/games?gameId=${ 1 }`, {
       'transports': [ 'websocket' ],
       'reconnectionDelay': 10000
     } );
